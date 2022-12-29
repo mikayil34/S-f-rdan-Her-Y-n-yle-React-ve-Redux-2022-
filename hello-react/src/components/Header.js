ceb3db7320cbab44e7ee08e93cc91a8320e07f21
Mikayil Aktemur <mikayil34@gmail.com>
2022-12-29 19:54:53 +0300

    edit

--- hello-react/src/components/Header.js
+++ hello-react/src/components/Header.js
@@ -1,4 +1,9 @@
 function Header() {
-  return <div>Merhaba Ben Header Bileşeneyim</div>;
+  return <div>Merhaba Ben Header Bileşeneyim
+
+    <p>Esse eiusmod occaecat laboris Lorem consectetur nisi sunt elit dolor laborum consequat ad deserunt incididunt. Commodo enim dolore dolore sit occaecat duis aliquip laboris eu sit cillum. Est adipisicing nisi proident anim sit et elit nostrud anim non. Eiusmod enim et eiusmod cupidatat magna irure sunt exercitation Lorem dolore cupidatat elit. Ea culpa labore adipisicing labore laborum officia pariatur elit magna nulla. Culpa esse enim cupidatat ipsum laborum officia adipisicing esse pariatur exercitation amet. Adipisicing culpa proident dolor ea consectetur consectetur culpa dolore commodo.
+
+Veniam in irure laborum duis sint adipisicing consequat voluptate. Ipsum culpa dolor velit pariatur ea et. Tempor ex officia ad sit velit aliquip in dolore consectetur pariatur minim fugiat velit id.</p>
+  </div>;
 }
 export default Header;
